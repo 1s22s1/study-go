e61141c3319dc53bfcf9f59611d790e27c83edbc
1s22s1 <1s22s1@example.com>
2023-08-30 07:58:30 +0900

    Add more color

--- irotsukegakari.go
+++ irotsukegakari.go
@@ -7,13 +7,26 @@ type Color int
 const (
 	Black Color = iota
 	Red
+	Green
+	Yellow
+	Blue
+	Magenta
+	Cyan
+	White
 )
 
+var m = map[Color]string{
+	Black:   "30",
+	Red:     "31",
+	Green:   "32",
+	Yellow:  "33",
+	Blue:    "34",
+	Magenta: "35",
+	Cyan:    "36",
+	White:   "37",
+}
+
 func irotsuke(s string, c Color) string {
-	m := map[Color]string{
-		Black: "30",
-		Red:   "31",
-	}
 
 	if val, ok := m[c]; ok {
 		return "\\e[" + val + s + "\\e[0m"

8354f192c464d113130bafd8ef8096944160fde7
1s22s1 <1s22s1@example.com>
2023-09-01 07:13:18 +0900

    Pass test

--- doguu/doguu.go
+++ doguu/doguu.go
@@ -1,16 +1,23 @@
 package doguu
 
+import "math"
+
 func arithmeticMean(x []int) float64 {
 	sum := 0
 
-	for _, n := range x {
-		sum += n
+	for _, v := range x {
+		sum += v
 	}
 
 	return float64(sum) / float64(len(x))
 }
 
 func geometricMean(x []float64) float64 {
+	n := 1.0
 
-	return float64(1)
+	for _, v := range x {
+		n *= v
+	}
+
+	return math.Pow(n, 1.0/float64(len(x)))
 }
